f894a6a81abe0c8f8e9fcedd34f2c5fb40a6a579
kakabisht <kakabisht07@gmail.com>
2020-08-08 23:48:24 +0530

    React Components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import './App.css';
 
-function App() {
-  return (
-    <div >
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">
-            Hridyesh Singh Bisht
-          </NavbarBrand>
-        </div>
-      </Navbar>
-      <h2>Hello World</h2>
-      <Menu />
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render(){
+    return (
+      <div >
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">
+              Hridyesh Singh Bisht
+            </NavbarBrand>
+          </div>
+        </Navbar>
+        <h2>Hello World</h2>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    )
 
-export default App;
+  }
+
+}
+export default App
