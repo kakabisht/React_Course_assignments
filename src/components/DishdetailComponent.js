a0a6190b02f58c1a2ef2bbe7a671506ae69820ce
kakabisht <kakabisht07@gmail.com>
2020-08-11 11:41:21 +0530

    Second assignment, Single page applications along with react router

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
-
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
     
     function RenderDish({dish}) {
         if (dish != null) {
@@ -52,19 +53,24 @@ import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
     const DishDetail = (props) => {
         if (props.dish != null) {
             return (
-                <div className="container">
+                    <div className="container">
                     <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
                         <div className="col-12">
                             <h3>{props.dish.name}</h3>
                             <hr />
-                        </div>    
+                        </div>                
                     </div>
                     <div className="row">
-                        <RenderDish dish={props.dish} />
-                        <RenderComments comments={props.dish.comments} />
-                    </div>    
-                </div>
-            );
+                            <RenderDish dish={props.dish} />
+                            <RenderComments comments={props.comments} />
+                    </div>
+                    </div>
+                );
+                
         } else {
             return (
                 <div></div>
